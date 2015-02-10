ea33505554df6796708059b0e80618241cb28dd2
Antonio Pierro <antonio.pierro@gmail.com>
2015-02-10 18:17:51 +0100

    Added Intent to send a message

--- app/src/main/java/com/example/antonio/androiddevelopment/DisplayMessageActivity.java
+++ app/src/main/java/com/example/antonio/androiddevelopment/DisplayMessageActivity.java
@@ -1,9 +1,11 @@
 package com.example.antonio.androiddevelopment;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class DisplayMessageActivity extends Activity {
@@ -11,7 +13,15 @@ public class DisplayMessageActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = new TextView(this);
+
+        textView.setTextSize(40);
+        textView.setText(message );
+
+        setContentView(textView);
     }
 
 
