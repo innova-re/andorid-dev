f2285214d86c96ba17db2ec9d30dfc653396e484
Antonio Pierro <antonio.pierro@gmail.com>
2015-02-11 13:35:43 +0100

    Added seekBarChangeListener

--- app/src/main/java/com/example/antonio/androiddevelopment/MainActivity.java
+++ app/src/main/java/com/example/antonio/androiddevelopment/MainActivity.java
@@ -4,10 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.SeekBar;
 
 public class MainActivity extends Activity {
 
@@ -15,6 +19,20 @@ public class MainActivity extends Activity {
     Button btnBlue;
     LinearLayout background;
     public final static String EXTRA_MESSAGE = "com.example.antonio.android-development";
+
+    private static final String TOTAL_BILL = "TOTAL_BILL";
+    private static final String CURRENT_TIP = "CURRENT_TIP";
+    private static final String BILL_WITHOUT_TIP = "BILL_WITHOUT_TIP";
+
+    private double billBeforeTip;
+    private double tipAmount;
+    private double finalBill;
+
+    EditText billBeforeTipET;
+    EditText tipAmountET;
+    EditText finalBillET;
+
+    SeekBar tipSeekBar;
 
     public void sendMessage(View view) {
 
@@ -34,6 +52,27 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if(savedInstanceState == null) {
+            // Initialize values with default values
+            billBeforeTip = 0.0;
+            tipAmount = .15;
+            finalBill = 0.0;
+        } else {
+            // Restore values from saved state
+            billBeforeTip = savedInstanceState.getDouble(BILL_WITHOUT_TIP);
+            tipAmount = savedInstanceState.getDouble(CURRENT_TIP);
+            finalBill = savedInstanceState.getDouble(TOTAL_BILL);
+        }
+
+        billBeforeTipET = (EditText) findViewById(R.id.billEditText);
+        tipAmountET = (EditText) findViewById(R.id.tipEditText);
+        finalBillET = (EditText) findViewById(R.id.finalBillEditText);
+
+        tipSeekBar = (SeekBar) findViewById(R.id.changeTipSeekBar);
+        tipSeekBar.setOnSeekBarChangeListener(tipSeekBarListener);
+
+        billBeforeTipET.addTextChangedListener(billBeforeTipListener);
+
         background = (LinearLayout) findViewById(R.id.background);
         btnGreen = (Button) findViewById(R.id.btnGreen);
         btnBlue = (Button) findViewById(R.id.btnBlue);
@@ -51,5 +90,64 @@ public class MainActivity extends Activity {
                 background.setBackgroundColor(Color.parseColor("#006699"));
             }
         });
+    }
+
+    private OnSeekBarChangeListener tipSeekBarListener = new OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            tipAmount = (tipSeekBar.getProgress()) * .01;
+            tipAmountET.setText(String.format("%.02f", tipAmount));
+            updateTipAndFinalBill();
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+
+    private TextWatcher billBeforeTipListener = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            try {
+                billBeforeTip = Double.parseDouble(s.toString());
+            } catch (NumberFormatException e) {
+                billBeforeTip = 0.0;
+            }
+
+            updateTipAndFinalBill();
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
+    private void updateTipAndFinalBill() {
+
+        double tipAmount = Double.parseDouble(tipAmountET.getText().toString());
+
+        double finalBill = billBeforeTip + (billBeforeTip * tipAmount);
+
+        finalBillET.setText(String.format("%.02f", finalBill));
+    }
+
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putDouble(TOTAL_BILL, finalBill);
+        outState.putDouble(CURRENT_TIP, tipAmount);
+        outState.putDouble(BILL_WITHOUT_TIP, billBeforeTip);
     }
 }
